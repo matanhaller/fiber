0f7f2ae92b98aaa3e73d1ad41268f19e1dd80ce1
Matan Haller <matan.haller@gmail.com>
2021-08-10 12:33:33 +0300

    Add script for plotting force

--- Dynamic/plotForce.m
+++ Dynamic/plotForce.m
@@ -7,16 +7,17 @@ clc;
 M = 10;
 
 x0 = 0; y0 = logspace(log10(1.02), 1, M); z0 = 0;
-beta = 0.09;
+beta = 0.1;
 
-kz = linspace(-1, 1, 40);
-omega = linspace(-0.5, 0.5, 40);
+kz = linspace(-8, 8, 200);
+omega = linspace(-6, 6, 200);
 [K, W] = meshgrid(kz, omega);
-N = -5:5;
+N = -10:10;
 
 dkz = kz(2) - kz(1);
 domega = omega(2) - omega(1);
 
+%% Plotting force as function of distance from cylinder
 figure; hold on;
 
 tic
@@ -70,3 +71,64 @@ end
 toc
 
 legend('FontSize', 14, 'Interpreter', 'latex');
+
+%% Plotting force as function of velocity
+rho = 1.5;
+M = 4;
+F = zeros(1, M);
+
+figure; hold on;
+
+tic
+for epsilon=[1.2, 2, 4, 12]
+    betaC = 1 / sqrt(epsilon);
+    gbC = betaC / sqrt(1 - betaC^2);
+    gb = [0.5, 1, 2, 4] * gbC; 
+    betas = sqrt(gb.^2 ./ (gb.^2 + 1));
+    for i=1:numel(betas)
+        beta = betas(i);
+        disp(beta);
+
+        EzInv = 0;
+        EphiInv = 0;
+        ErhoInv = 0;
+        eta0HzInv = 0;
+        eta0HphiInv = 0;
+        eta0HrhoInv = 0;
+
+        for n=N
+            disp(n);
+
+            [Ank, Bnk, eta0Cnk, eta0Dnk] = secondaryFieldCoeffs(n, K, W, x0, rho, z0, beta, epsilon);
+            EzFourier = EzSecondaryFourier(Ank, Bnk, eta0Cnk, eta0Dnk, rho, n, K, W, x0, rho, z0, beta, epsilon);
+            EphiFourier = EphiSecondaryFourier(Ank, Bnk, eta0Cnk, eta0Dnk, rho, n, K, W, x0, rho, z0, beta, epsilon);            
+            ErhoFourier = ErhoSecondaryFourier(Ank, Bnk, eta0Cnk, eta0Dnk, rho, n, K, W, x0, rho, z0, beta, epsilon);
+            eta0HzFourier = eta0HzSecondaryFourier(Ank, Bnk, eta0Cnk, eta0Dnk, rho, n, K, W, x0, rho, z0, beta, epsilon);
+            eta0HphiFourier = eta0HphiSecondaryFourier(Ank, Bnk, eta0Cnk, eta0Dnk, rho, n, K, W, x0, rho, z0, beta, epsilon);
+            eta0HrhoFourier = eta0HrhoSecondaryFourier(Ank, Bnk, eta0Cnk, eta0Dnk, rho, n, K, W, x0, rho, z0, beta, epsilon);
+
+            EzTotal = sum(sum(EzFourier)) * dkz * domega;
+            EphiTotal = sum(sum(EphiFourier)) * dkz * domega;
+            ErhoTotal = sum(sum(ErhoFourier)) * dkz * domega;
+            eta0HzTotal = sum(sum(eta0HzFourier)) * dkz * domega;
+            eta0HphiTotal = sum(sum(eta0HphiFourier)) * dkz * domega;
+            eta0HrhoTotal = sum(sum(eta0HrhoFourier)) * dkz * domega;
+
+            EzInv = EzInv + (-1j)^n*EzTotal;
+            EphiInv = EphiInv + (-1j)^n*EphiTotal;
+            ErhoInv = ErhoInv + (-1j)^n*ErhoTotal;
+            eta0HzInv = eta0HzInv + (-1j)^n*eta0HzTotal;
+            eta0HphiInv = eta0HphiInv + (-1j)^n*eta0HphiTotal;
+            eta0HrhoInv = eta0HrhoInv + (-1j)^n*eta0HrhoTotal;
+        end
+
+        F(i) = sqrt(abs(EzInv + eta0HrhoInv).^2 + abs(EphiInv).^2 + abs(ErhoInv - eta0HzInv).^2);
+    end
+    plot(gb./gbC, F/F(1), '--o', 'LineWidth', 1, 'DisplayName', sprintf('$\\varepsilon=%.1f$', epsilon));
+end
+toc
+
+xlabel('$\gamma \beta / (\gamma \beta)_\mathrm{C}$', 'FontSize', 14, 'Interpreter', 'latex');
+ylabel('$|\bar{F}|$', 'FontSize', 14, 'Interpreter', 'latex');
+legend('FontSize', 14, 'Interpreter', 'latex');
+
